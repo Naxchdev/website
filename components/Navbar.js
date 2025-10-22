94d0f281566d6bf54d97a05b7050663111857979
NoOnceMore.Dev <122416415+NoOnceMore@users.noreply.github.com>
2025-10-22 21:30:38 +0700

    Initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="w-full flex bg-white text-black">
+            <div>Home</div>
+            <div>About me</div>
+            <div>Certificate</div>
+            <div>Skill</div>
+        </div>
+    </nav>
+  )
+}
+export default Navbar
